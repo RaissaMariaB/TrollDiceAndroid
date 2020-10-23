01b43dcac1e25764fe206f54141c7147afde9542
RaissaBarata <raissapereira@sas.com.br>
2020-10-22 22:12:16 -0300

    Add images of dices and change the type of view on main activity

--- app/src/main/java/com/wwc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wwc/lancedados/MainActivity.kt
@@ -3,7 +3,7 @@ package com.wwc.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +11,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonRollDice = findViewById<Button>(R.id.buttonDice)
-        var valueDice1 = findViewById<TextView>(R.id.dice1)
-        var valueDice2 = findViewById<TextView>(R.id.dice2)
+        var imageDice1 = findViewById<ImageView>(R.id.dice1)
+        var valueDice2 = findViewById<ImageView>(R.id.dice2)
 
         buttonRollDice.setOnClickListener {
-            valueDice1.text = dropDice().toString()
-            valueDice2.text = dropDice().toString()
+
         }
     }
 
