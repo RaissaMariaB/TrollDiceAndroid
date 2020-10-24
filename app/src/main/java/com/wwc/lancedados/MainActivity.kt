d4fb48cf75ad6597c66e1c518464dd9f943dd7b9
RaissaBarata <raissapereira@sas.com.br>
2020-10-23 22:15:11 -0300

    Add dynamic images to Dices on layout with when() method and setImageResource()

--- app/src/main/java/com/wwc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wwc/lancedados/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +13,28 @@ class MainActivity : AppCompatActivity() {
 
         val buttonRollDice = findViewById<Button>(R.id.buttonDice)
         var imageDice1 = findViewById<ImageView>(R.id.dice1)
-        var valueDice2 = findViewById<ImageView>(R.id.dice2)
+        var imageDice2 = findViewById<ImageView>(R.id.dice2)
+
 
         buttonRollDice.setOnClickListener {
-
+            val imageNumberDice1: Int = imagePicker(dropDice())
+            val imageNumberDice2: Int = imagePicker(dropDice())
+            imageDice1.setImageResource(imageNumberDice1)
+            imageDice2.setImageResource(imageNumberDice2)
         }
+    }
+
+    private fun imagePicker(num: Int): Int {
+        return when(num) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+
     }
 
     fun dropDice(): Int {
