1064d66f9de58d8bd96779679ea9987e8c9d2909
RaissaBarata <raissapereira@sas.com.br>
2020-10-24 15:34:56 -0300

    Add beckup of project with null input

--- app/src/main/java/com/wwc/lancedados/RegisterActivity.kt
+++ app/src/main/java/com/wwc/lancedados/RegisterActivity.kt
@@ -1,11 +1,26 @@
 package com.wwc.lancedados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        var playButton = findViewById<Button>(R.id.playButton)
+        val playerName = findViewById<EditText>(R.id.textInputPersonName).text.toString()
+        val playerName2 = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()
+
+
+        playButton.setOnClickListener {
+             var intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("playerName", playerName)
+
+            startActivity(intent)
+        }
     }
 }
